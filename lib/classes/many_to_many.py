d22739c920a5fdedee0aeff154353c50ad44225a
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-09-06 12:53:03 +0300

    Cleared initial errors in class customer

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -40,6 +40,18 @@ class Customer:
     def __init__(self, name):
         self.name = name
         
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise Exception('Name must be a string')
+        if not (1<= len(value) <= 15):
+            raise Exception('Name must be between 1 to 15 characters long')
+        self._name = value
+ 
     def orders(self):
         pass
     
