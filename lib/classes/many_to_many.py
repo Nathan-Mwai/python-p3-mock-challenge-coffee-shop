3da07797cea784eccb269bb9980d68b289151a0a
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-09-06 12:38:25 +0300

    first coffee errors fixed

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,7 +1,29 @@
 class Coffee:
     def __init__(self, name):
+        self._name = None
         self.name = name
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        # Value will be the placeholder of all names to be filled
         
+        # Checks if name is not none
+        if hasattr(self, '_name') and self._name is not None:
+            raise Exception('Name cannot be changed once set')
+        
+        # Checks if name is a string
+        if not isinstance(value, str):
+            raise Exception('Name must be a string')
+        
+        # Checks if length of the coffee name is 3 or more 
+        if len(value) < 3:
+            raise Exception('Name must be at least 3 characters long')
+        self._name = value
+    
     def orders(self):
         pass
     
